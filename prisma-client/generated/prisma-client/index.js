5a4ee5c7cc761c1cc2cf406a42d8ba6a183960c2
gelinas <jlgelinas@gmail.com>
2019-12-13 18:08:31 -0500

    moved prisma endpoint to env variable

--- prisma-client/generated/prisma-client/index.js
+++ prisma-client/generated/prisma-client/index.js
@@ -7,39 +7,11 @@ var models = [
   {
     name: "User",
     embedded: false
-  },
-  {
-    name: "Event",
-    embedded: false
-  },
-  {
-    name: "Event_Image",
-    embedded: false
-  },
-  {
-    name: "Event_Url",
-    embedded: false
-  },
-  {
-    name: "Location",
-    embedded: false
-  },
-  {
-    name: "Organization",
-    embedded: false
-  },
-  {
-    name: "Neighborhood",
-    embedded: false
-  },
-  {
-    name: "Geo_Json",
-    embedded: false
   }
 ];
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: `https://mark-ap-106dd265de.herokuapp.com/prisma-client/dev`
+  endpoint: `https://gelinas-dev-cc-6c61dd9e6c.herokuapp.com/prisma-client/dev`
 });
 exports.prisma = new exports.Prisma();
