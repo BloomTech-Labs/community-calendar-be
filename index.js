6f6a750f5538fbce9489cd15615dba44d9412fa1
Mark King <markpkng@gmail.com>
2019-12-16 17:56:09 -0500

    remove dotenv for production

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const {ApolloServer} = require('apollo-server');
 const {Prisma} = require('./prisma-client/generated/prisma-client');
 const typeDefs = require('./graphql/schema');
